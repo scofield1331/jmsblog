674ae2d6bfbd5ba82403edc55550cb1c0932ef87
pham van hung <phamminhhung1996@gmail.com>
2018-01-18 17:35:34 +0700

    ok

--- views/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.more li').click(function(){
-        $(this).closest('.catmenu').children('li').removeClass('active');
-    })
-    $('.catmenu>li').click(function(){
-        $(this).closest('.catmenu').find('.more li').removeClass('active');
-    })
-})
